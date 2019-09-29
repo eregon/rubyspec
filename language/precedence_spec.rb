39ff9ee6fb1bbe394a677dea316cd1dbbe824fa2
Benoit Daloze <eregontp@gmail.com>
2019-09-29 14:16:09 +0200

    Re-introduce "Remove warnings of flip-flop deprecation from tests and specs" for 2.6
    * This reverts commit 574b5c05fa17955abecfe0cd4cfe3e1866b2063d.
    * The warnings still need to be suppressed for specs on 2.6.3.

--- language/precedence_spec.rb
+++ language/precedence_spec.rb
@@ -296,14 +296,16 @@ describe "Operators" do
     -> { eval("1...2...3")  }.should raise_error(SyntaxError)
   end
 
- it ".. ... have higher precedence than ? :" do
-   # Use variables to avoid warnings
-   from = 1
-   to = 2
-   # These are flip-flop, not Range instances
-   (from..to ? 3 : 4).should == 3
-   (from...to ? 3 : 4).should == 3
- end
+  it ".. ... have higher precedence than ? :" do
+    # Use variables to avoid warnings
+    from = 1
+    to = 2
+    # These are flip-flop, not Range instances
+    suppress_warning do
+      (from..to ? 3 : 4).should == 3
+      (from...to ? 3 : 4).should == 3
+    end
+  end
 
   it "? : is right-associative" do
     (true ? 2 : 3 ? 4 : 5).should == 2
